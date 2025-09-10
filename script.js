66b1759524cd4fb57eb8d91cab2007a2c621504b
pedrincaneladefogo <pedro.silva.camargo@escola.pr.gov.br>
2025-09-10 20:57:49 -0300

    ok

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Assim que saiu do colegio voce se depara com uma nova tecnologia, um chat que consegue responder todas as duvidas que uma pessoa pode ter, ele tambem gera imagens e audios. Qual é o seu preimeiro pensamento?",
+        alternativas:[
+            {
+         texto: "Isso é assustador!",
+         afirmacao:"No inicio, eu ficaria com medo do que essa tecnologia pode fazer."
+
+            },
+            {
+                
+            }
+        ]
+    }
+]
+
